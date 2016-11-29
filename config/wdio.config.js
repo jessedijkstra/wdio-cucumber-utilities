c06f54fded6bbeb8524de9bb5a64e520de18aacc
Jesse Dijkstra <mail@jessedijkstra.nl>
2016-11-29 15:16:26 +0100

    Add debug ENV var to config

--- config/wdio.config.js
+++ config/wdio.config.js
@@ -1,3 +1,10 @@
 const { config } = require('../lib/config.js');
 
+if (process.env.DEBUG) {
+  const timeout = (1000 * 60 * 60 * 24);
+  config.debug = true;
+  config.waitforTimeout = timeout;
+  config.cucumberOpts.timeout = timeout;
+}
+
 exports.config = config;
